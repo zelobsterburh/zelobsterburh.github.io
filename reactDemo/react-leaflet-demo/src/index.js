5d83f1acd36b712e91ac6251c8b99892f1120a57
Bret Jones <bretcj1@gmail.com>
2023-02-23 16:20:18 -0500

    It works

---
+++ reactDemo/react-leaflet-demo/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
+import './index.css';
+
+function App() {
+	return (
+		<div>
+			<div>
+				<h1>Howdy</h1>
+				<p>I'm testing things</p>
+			</div>
+			<MapContainer id="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+			</MapContainer>
+		</div>
+	);
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
