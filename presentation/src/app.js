c11e703df57edd3cecb8eb087dc7f9a998f5aa39
Bret Jones <bretcj1@gmail.com>
2023-03-30 12:55:24 -0400

    Another map

--- presentation/src/app.js
+++ presentation/src/app.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
+
+import { Marker } from "react-leaflet/Marker";
+import { Popup } from "react-leaflet/Popup";
 import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
 import { LayersControl } from "react-leaflet/LayersControl";
 import { Pane } from "react-leaflet/Pane";
+
 import { useMap } from "react-leaflet/hooks";
 
 import "./css/style.css";
@@ -11,6 +15,18 @@ import "./css/style.css";
 export default function App() {
 	return (
 		<div className="container">
+			{/* Basic with marker */}
+			<MapContainer center={[40, -75]} zoom={13} scrollWheelZoom={false}>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker position={[40, -75]}>
+					<Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup>
+				</Marker>
+			</MapContainer>
+
+			{/* Multiple Tile Layers */}
 			<MapContainer id="map" center={[35, -75]} zoom={5}>
 				<LayersControl position="topleft">
 					<Pane name="base" style={{ zIndex: 1 }}>
